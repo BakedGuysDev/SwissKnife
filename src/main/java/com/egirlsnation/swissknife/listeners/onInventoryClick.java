a795d23852ffa809b067575c65d7c4c4ff91a4eb
Lerbiq <57460117+Lerbiq@users.noreply.github.com>
2021-01-08 19:21:07 +0100

    Initial commit

--- src/main/java/com/egirlsnation/swissknife/listeners/onInventoryClick.java
+++ src/main/java/com/egirlsnation/swissknife/listeners/onInventoryClick.java
@@ -8,7 +8,6 @@ import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class onInventoryClick implements Listener {
