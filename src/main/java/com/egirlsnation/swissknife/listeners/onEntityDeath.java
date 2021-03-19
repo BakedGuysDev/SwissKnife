b3ca945e21c70bb616f632af6109698bb84f6c66
killmlana <killmlana@egirlsnation.com>
2021-03-19 19:18:25 +0530

    Optimized

--- src/main/java/com/egirlsnation/swissknife/listeners/onEntityDeath.java
+++ src/main/java/com/egirlsnation/swissknife/listeners/onEntityDeath.java
@@ -17,16 +17,17 @@ public class onEntityDeath implements Listener {
     @EventHandler
     public void onEntityDeath(EntityDamageEvent e) {
         LivingEntity pet = (LivingEntity) e.getEntity();
-        if (pet.getHealth() - e.getDamage() <= 0) { //Bukkit.broadcastMessage("pet has died");
-            if ((pet instanceof Tameable) && (pet instanceof Sittable)) { //Bukkit.broadcastMessage("pet is sittable");
-                if ((((Tameable) pet).isTamed())) { //Bukkit.broadcastMessage("pet's is tamed");
+        if ((pet instanceof Tameable) && (pet instanceof Sittable)) { //Bukkit.broadcastMessage("pet has died");
+            if ((((Tameable) pet).isTamed())) { //Bukkit.broadcastMessage("pet is sittable");
+                if (pet.getHealth() - e.getDamage() <= 0) { //Bukkit.broadcastMessage("pet's is tamed");
                     if (((Tameable) pet).getOwner() instanceof Player) {
-                        if (((Tameable) pet).getOwner() != null) {
+                        Player p = (Player) ((Tameable) pet).getOwner();
+                        if (p != null) {
                             //Bukkit.broadcastMessage("pet's owner is online");
-                            if (((Player) ((Tameable) pet).getOwner()).getInventory().getItemInOffHand().getType().equals(Material.TOTEM_OF_UNDYING)) { //Bukkit.broadcastMessage("owner has totem in offhand");
-                                ((Player) ((Tameable) pet).getOwner()).getInventory().getItemInOffHand().setAmount(((Player) ((Tameable) pet).getOwner()).getInventory().getItemInOffHand().getAmount() - 1);
-                                ((Player) ((Tameable) pet).getOwner()).playEffect(EntityEffect.TOTEM_RESURRECT);
-                                ((Player) ((Tameable) pet).getOwner()).sendMessage(ChatColor.AQUA + "Your pet used a Totem.");
+                            if ((p.getInventory().getItemInOffHand().getType().equals(Material.TOTEM_OF_UNDYING))) { //Bukkit.broadcastMessage("owner has totem in offhand");
+                                p.getInventory().getItemInOffHand().setAmount((p.getInventory().getItemInOffHand().getAmount() - 1));
+                                p.playEffect(EntityEffect.TOTEM_RESURRECT);
+                                p.sendMessage(ChatColor.AQUA + "Your pet used a Totem.");
                                 e.setCancelled(true);
                                 pet.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 900, 2));
                                 pet.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 800, 1));
