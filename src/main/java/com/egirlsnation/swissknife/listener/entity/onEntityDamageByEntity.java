2d18dd21e0d40a88c0de5827338599757ef91e5b
Lerbiq <57460117+Lerbiq@users.noreply.github.com>
2021-08-11 22:02:24 +0200

    Added license headers, fixed TPS alert and misc formatting

--- src/main/java/com/egirlsnation/swissknife/listener/entity/onEntityDamageByEntity.java
+++ src/main/java/com/egirlsnation/swissknife/listener/entity/onEntityDamageByEntity.java
@@ -1,3 +1,15 @@
+/*
+ * This file is part of the SwissKnife plugin distibution  (https://github.com/EgirlsNationDev/SwissKnife).
+ * Copyright (c) 2021 Egirls Nation Development
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * You should have received a copy of the MIT
+ * License along with this program.  If not, see
+ * <https://opensource.org/licenses/MIT>.
+ */
+
 package com.egirlsnation.swissknife.listener.entity;
 
 import com.egirlsnation.swissknife.util.CombatCheck;
@@ -10,45 +22,57 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
+import static com.egirlsnation.swissknife.SwissKnife.Config.kickOnHighDamage;
+import static com.egirlsnation.swissknife.SwissKnife.Config.preventHighDmg;
+
 public class onEntityDamageByEntity implements Listener {
 
     private final CombatCheck combatCheck = new CombatCheck();
 
     @EventHandler
-    private void onEntityDamageByEntity(EntityDamageByEntityEvent e){
-        if(e.getDamager() instanceof Player){
+    private void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof Player) {
             Player player = (Player) e.getDamager();
-            if(player.isOp()) return;
-            if(e.getDamage() > 1000){
+            if (e.getEntity().getPassengers().contains(player)) {
                 e.setCancelled(true);
-                player.damage(e.getDamage());
-                player.kickPlayer("Oi cunt, what the bloody hell");
+                return;
+            }
+            if (preventHighDmg) {
+                if (player.isOp()) return;
+                if (e.getDamage() > 1000) {
+                    e.setCancelled(true);
+                    player.damage(e.getDamage());
+                    if(kickOnHighDamage){
+                        player.kickPlayer("Oi cunt, what the bloody hell");
+                    }
+                }
             }
         }
 
-        if(!(e.getEntity() instanceof Player)) return;
+        if (!(e.getEntity() instanceof Player)) return;
         Player player = (Player) e.getEntity();
         EntityDamageEvent.DamageCause cause = e.getCause();
         Entity damager = e.getDamager();
 
 
         //Checking if the damage is caused by an explosion from tnt or end crystal (Anchor check in
-        if(cause.equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION)){
-            if(!damager.getType().equals(EntityType.ENDER_CRYSTAL) && !damager.getType().equals(EntityType.PRIMED_TNT) && !damager.getType().equals(EntityType.SNOWBALL)) return;
+        if (cause.equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION)) {
+            if (!damager.getType().equals(EntityType.ENDER_CRYSTAL) && !damager.getType().equals(EntityType.PRIMED_TNT) && !damager.getType().equals(EntityType.SNOWBALL))
+                return;
             combatCheck.addToCombatMap(player);
             return;
         }
 
         //Checking if the damage
-        if(cause.equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK) || cause.equals(EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK)){
-            if(!(damager instanceof Player)) return;
+        if (cause.equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK) || cause.equals(EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK)) {
+            if (!(damager instanceof Player)) return;
             combatCheck.addToCombatMap(player);
             return;
         }
 
-        if(cause.equals(EntityDamageEvent.DamageCause.PROJECTILE)){
+        if (cause.equals(EntityDamageEvent.DamageCause.PROJECTILE)) {
             Projectile projectile = (Projectile) damager;
-            if(!(projectile.getShooter() instanceof Player)) return;
+            if (!(projectile.getShooter() instanceof Player)) return;
             combatCheck.addToCombatMap(player);
         }
 
--- src/main/java/com/egirlsnation/swissknife/listener/player/onPlayerInteractEntity.java
+++ src/main/java/com/egirlsnation/swissknife/listener/player/onPlayerInteractEntity.java
@@ -1,6 +1,17 @@
+/*
+ * This file is part of the SwissKnife plugin distibution  (https://github.com/EgirlsNationDev/SwissKnife).
+ * Copyright (c) 2021 Egirls Nation Development
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * You should have received a copy of the MIT
+ * License along with this program.  If not, see
+ * <https://opensource.org/licenses/MIT>.
+ */
+
 package com.egirlsnation.swissknife.listener.player;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -13,7 +24,6 @@ public class onPlayerInteractEntity implements Listener {
     private void PlayerInteractAtEntity(PlayerInteractEntityEvent e){
         if(!e.getRightClicked().getType().equals(EntityType.ENDER_CRYSTAL)) return;
         if(e.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.NAME_TAG)){
-            Bukkit.getLogger().info(e.getRightClicked().getCustomName());
             e.setCancelled(true);
             return;
         }
